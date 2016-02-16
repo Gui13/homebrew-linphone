3df4ca3da71df9441d369863d3d0cfdddee71cbc
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2016-02-16 15:49:08 +0100

    Use cmake for ortp

--- ortp.rb
+++ ortp.rb
@@ -3,19 +3,14 @@ require "formula"
 class Ortp < Formula
 	desc "oRTP library"
 	homepage "http://www.linphone.org/technical-corner/ortp/"
-	url "http://download.savannah.gnu.org/releases/linphone/ortp/sources/ortp-0.25.0.tar.gz"
-	sha256 "0deb826b79b1fd329f7638821d5439dc343bc55bb122daa6c90d19116cfe8d9a"
+	url "https://github.com/BelledonneCommunications/ortp/archive/0.25.0.tar.gz"
+	sha256 "cc76bf057b362332a0b3143d4d1fdf6d8651fb2f161f30b575071665e9dac231"
 
-	depends_on "pkg-config" => :build
-	depends_on "libtool" => :build
-	depends_on "intltool" => :build
-	depends_on "autoconf" => :build
-	depends_on "automake" => :build
+	depends_on "cmake" => :build
 	depends_on "openssl"
 
 	def install
-		system "./autogen.sh"
-		system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+		system "cmake", ".", *std_cmake_args
 		system "make", "install"
 		
 	end
