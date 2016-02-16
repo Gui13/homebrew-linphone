98b491971973d73fe094832acfaed4b6eb32f5e0
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2016-02-16 15:50:08 +0100

    Use cmake for ortp and add HEAD

--- ortp.rb
+++ ortp.rb
@@ -5,6 +5,7 @@ class Ortp < Formula
 	homepage "http://www.linphone.org/technical-corner/ortp/"
 	url "https://github.com/BelledonneCommunications/ortp/archive/0.25.0.tar.gz"
 	sha256 "cc76bf057b362332a0b3143d4d1fdf6d8651fb2f161f30b575071665e9dac231"
+	head "https://github.com/BelledonneCommunications/ortp.git"
 
 	depends_on "cmake" => :build
 	depends_on "openssl"
